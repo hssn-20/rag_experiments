7ea775d07dc44da9eddcc6da9c0e283f7b3fbe4c
hssn-20 <69965978+hssn-20@users.noreply.github.com>
2024-01-21 23:27:40 +0000

    webpage

--- app.py
+++ app.py
@@ -34,8 +34,7 @@ def add_data_to_app():
     csv_file = StringIO(response.text)
     for row in csv.reader(csv_file):
         if row and row[0] != "url":
-            print(row[0])
-            app.add(row[0], data_type='web_page')
+            app.add(row[0], data_type="web_page")
 
 
 app = sadhguru_ai()
